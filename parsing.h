86322a5348096487e83f4c75f1ecc3a0aca4e27d
zylee3 <41247049S@gapps.ntnu.edu.tw>
2024-06-08 08:08:52 -0700

    parsing library

--- parsing.h
+++ parsing.h
@@ -1,51 +1,130 @@
-#include <stdio.h>
+#ifndef parsing_h
+#define parsing_h
+
 #include <stdint.h>
 
-typedef struct _sID
+#define MAX_FOUND 16
+
+enum StructId
 {
-	uint8_t key;
-	char name[64];
-} ID;
+	NONE            = 0,
+	ID_PARENT_CHILD = 1,
+	NO_FOR_PRINT    = ID_PARENT_CHILD,
+	TABLE           = 2,
+	CONTAINER_START = TABLE,
+	ARRAY           = 3,
+	CONTAINER_END = ARRAY,
+	FINAL_PROJECT   = 4,
+	SCENE           = 5,
+	CHARACTER       = 6,
+	ITEM            = 7,
+	EVENT           = 8,
+	DIALOGUE        = 9,
+};
+
+struct _sContainer;
+
+typedef struct _sIdParentChild
+{
+	enum StructId           _id;
+	struct _sContainer*     _parent;
+	struct _sIdParentChild* _child;
+	struct _sIdParentChild* _next;
+} IdParentChild;
+
+typedef struct _sContainer
+{
+	enum StructId       _id;
+	struct _sContainer* _parent;
+	IdParentChild*      _child;
+	IdParentChild*      _next;
+	char*               name;
+} Container;
+
+typedef struct _sFinalProject
+{
+	enum StructId  _id;
+	Container*     _parent;
+	IdParentChild* _child;
+	IdParentChild* _next;
+	char*          name;
+	char*          author;
+} FinalProject;
 
 typedef struct _sScene
 {
-	ID* pID;
-	char background[64];
+	enum StructId  _id;
+	Container*     _parent;
+	IdParentChild* _child;
+	IdParentChild* _next;
+	char*          name;
+	char*          background;
 } Scene;
 
 typedef struct _sCharacter
 {
-	ID* pID;
-	char avatar[64];
-	char tachie[64];
+	enum StructId  _id;
+	Container*     _parent;
+	IdParentChild* _child;
+	IdParentChild* _next;
+	char*          name;
+	char*          avatar;
+	char*          tachie;
+	uint32_t       favor_cats;
+	uint32_t       favor_dogs;
 } Character;
 
 typedef struct _sItem
 {
-	ID* pID;
-	char image[64];
+	enum StructId  _id;
+	Container*     _parent;
+	IdParentChild* _child;
+	IdParentChild* _next;
+	char*          name;
+	char*          image;
 } Item;
 
-typedef struct _sFavor
+typedef struct _sEvent
 {
-	ID* pID;
-	uint32_t favor_cats;
-	uint32_t favor_dogs;
-} Favor;
+	enum StructId  _id;
+	Container*     _parent;
+	IdParentChild* _child;
+	IdParentChild* _next;
+	char*          scene;
+	char*          dialogue;
+} Event;
 
-typedef struct _sDialogueStory
+typedef struct _sDialogue
 {
-	ID* pID;
-	char text[1024];
-	char next[64];
-	char event[64];
-} DialogueStory;
+	enum StructId  _id;
+	Container*     _parent;
+	IdParentChild* _child;
+	IdParentChild* _next;
+	char*          character;
+	char*          text;
+	char*          next;
+	char*          event;
+	char*          item;
+} Dialogue;
 
-typedef struct _sDialogueConversation
+typedef struct _sFound
 {
-	ID* pID;
-	char character[64];
-	char text[1024];
-	char next[64];
-	char event[64];
-} DialogueConversation;
+	int32_t count; // negative for error
+	void*   pvMatchs[MAX_FOUND];
+} Found;
+
+typedef void* parsing_handle;
+
+parsing_handle script_init();
+
+int32_t script_parsing(parsing_handle, const char* tomlFile);
+
+Found script_find_record(parsing_handle, const char* tomlKey);
+
+void script_print_found(parsing_handle, Found*);
+
+void script_print_all(parsing_handle);
+
+int32_t script_uninit(parsing_handle);
+
+#endif
